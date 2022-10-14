02bd36b17eec5c982afb4054d935f7afed5cd5a8
TM-Horsfall <horsfall360@gmail.com>
2022-10-14 00:55:58 +0000

    Another correction

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,13 +1,12 @@
 #include <unistd.h>
 
 /**
-* main - Printing a text
+* main - Entry
 *
-* Return: Always 1 (Success)
+* Return: 1
 */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar,
-2015-10-19\n", 59);
-return (1);
+	write(1, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 59);
+	return (1);
 }
